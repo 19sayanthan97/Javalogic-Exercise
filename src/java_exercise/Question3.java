77e2d28b34edac419db432017d729e079c71e821
19sayanthan97 <sajanth80@gmail.com>
2023-01-11 12:00:48 +0530

    first commit

---
+++ src/java_exercise/Question3.java
@@ -0,0 +1,25 @@
+package java_exercise;
+
+public class Question3 {
+
+	public static void main(String[] args) {
+		
+int[] A = {15,85,59,58,59,89,89,89,89,78};
+	    
+		int total = 0;
+	
+		for(int i =0; i < A.length; i++ )
+		{
+		
+		total = total + A[i];
+			
+		
+		}
+		
+		
+		
+		System.out.println("Sum of array A is:" + total);
+
+	}
+
+}
